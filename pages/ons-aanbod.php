3082e51a9e6f87a7bf3f9e504679925b3477fa43
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2025-05-12 17:42:11 +0200

    Final fix (hopefully)

--- pages/ons-aanbod.php
+++ pages/ons-aanbod.php
@@ -1,7 +1,7 @@
-<?php require "../includes/header.php" ?>
+<?php require "includes/header.php" ?>
 
 <main>
     <h2>Ons aanbod</h2>
 </main>
-<?php require "../includes/footer.php" ?>
+<?php require "includes/footer.php" ?>
 
