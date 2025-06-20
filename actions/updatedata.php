380efd09305beb04a6aa0001ec46f9fcf6c3a2ab
QuintenJung <9024671@student.tcrmbo.nl>
2025-06-20 13:36:21 +0200

    klijne update

--- actions/updatedata.php
+++ actions/updatedata.php
@@ -2,31 +2,32 @@
 session_start();
 require "database/connection.php";
 
-    if (!empty($_POST['email'])) {
-        $check_account = $conn->prepare("SELECT * FROM account WHERE email = :email");
-        $check_account->bindParam(":email", $_POST['email']);
-        $check_account->execute();
+if (!empty($_POST['email'])) {
+    $check_account = $conn->prepare("SELECT * FROM account WHERE email = :email AND id != :id");
+    $check_account->bindParam(":email", $_POST['email']);
+    $check_account->bindParam(":id", $_SESSION['id']);
+    $check_account->execute();
 
-        if ($check_account->rowCount() === 0) {
-            $select_user = $conn->prepare("UPDATE account SET email = :email WHERE id = :id");
-            $select_user->bindParam(":id", $_SESSION['id']);
-            $select_user->bindParam(":email", $_POST['email']);
-            $select_user->execute();
-        } elseif ($check_account->rowCount() > 0) {
-            $_SESSION["error"] = "Email is al in gebruik.";
-            header("Location: usersettings.php");
-            exit();
-        }
-    }
-
-    if (!empty($_POST['password'])) {
-        $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-        $select_user = $conn->prepare("UPDATE account SET password = :password WHERE id = :id");
-        $select_user->bindParam(":password", $hashed_password);
+    if ($check_account->rowCount() === 0) {
+        $select_user = $conn->prepare("UPDATE account SET email = :email WHERE id = :id");
         $select_user->bindParam(":id", $_SESSION['id']);
+        $select_user->bindParam(":email", $_POST['email']);
         $select_user->execute();
+        $_SESSION["error"] = "gegevens geüpdatet";
+    } elseif ($check_account->rowCount() > 0) {
+        $_SESSION["error"] = "Email is al in gebruik.";
+        header("Location: usersettings.php");
+        exit();
     }
+}
 
-    header('Location: usersettings.php');
-    exit();
+if (!empty($_POST['password'])) {
+    $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $select_user = $conn->prepare("UPDATE account SET password = :password WHERE id = :id");
+    $select_user->bindParam(":password", $hashed_password);
+    $select_user->bindParam(":id", $_SESSION['id']);
+    $select_user->execute();
+}
 
+header('Location: usersettings.php');
+exit();
--- pages/usersettings.php
+++ pages/usersettings.php
@@ -18,16 +18,6 @@ try {
 }
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 <div class="data">
 <form action="settings-handler" class="account-form" method="post">
     <h2 class="login-title">Settings</h2>
